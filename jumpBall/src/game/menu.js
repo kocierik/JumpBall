028bdfd2ee0cf77083747fd7443714ed3be7f412
Koci Erik <41591336+kocierik@users.noreply.github.com>
2020-04-22 13:33:56 +0200

    add credits menu

--- jumpBall/src/game/menu.js
+++ jumpBall/src/game/menu.js
@@ -2,3 +2,12 @@
 function pauseMenu(){
     window.location = 'index.html';
 }
+function  informationMenu() {
+    window.location = 'information.html';
+}
+function  startMenu() {
+    window.location = 'startMenu.html';
+}
+function creditMenu() {
+    window.location = 'credit.html';
+}
